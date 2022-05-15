df6d325880963df24130ba0d942377543e0bfeba
brainsoup82 <chrisowens82@gmail.com>
2022-05-15 20:57:47 +0100

    Created GUI

--- Graphs/graphbuilder.py
+++ Graphs/graphbuilder.py
@@ -17,21 +17,23 @@ Paths are the list items within list for each key with format
 import copy
 import math
 
+
 class Graph:
-    '''
+    """
     Graphs are dictionaries with nodes as the keys
     Paths are the list items within list for each key with format
     ['end_node', distance]
-    '''
+    """
+
     def __init__(self):
-        '''
+        """
         Creates new empty graph
 
         Returns
         -------
         None.
 
-        '''
+        """
         self.graph = {}
         self.oneway = False
 
@@ -44,8 +46,8 @@ class Graph:
             message += '\n'
         return message
 
-    def create_node(self,node_name):
-        '''
+    def create_node(self, node_name):
+        """
         Adds a node to the graph if it does not yet exist
 
         Parameters
@@ -56,17 +58,26 @@ class Graph:
         -------
         None.
 
-        '''
+        """
 
         # Check if node already in graph, if so then pass
         # Otherwise add a key with a blank list ready to append connections to
         if node_name in self.graph.keys():
             pass
         else:
-            self.graph[node_name]=[]
+            self.graph[node_name] = []
 
-    def add_path(self,start_node, end_node, distance, oneway=False):
-        '''
+    def node_names(self):
+        """
+        Returns
+        -------
+        List of node names.
+
+        """
+        return list(self.graph.keys())
+
+    def add_path(self, start_node, end_node, distance, oneway=False):
+        """
         Adds a path to the graph - will add new nodes as found
 
         Parameters
@@ -81,23 +92,23 @@ class Graph:
         -------
         None.
 
-        '''
+        """
         # Ensure that both nodes are created in the graph
         self.create_node(start_node)
         self.create_node(end_node)
 
-        #append the new connection from start node to end node
+        # append the new connection from start node to end node
         self.graph[start_node].append([end_node, distance])
 
         # If the link is one way then the link is added.
-        # Otherwise add the reverse link
+        # Otherwise, add the reverse link
         if oneway is False:
             self.graph[end_node].append([start_node, distance])
         else:
             self.oneway = True
 
-    def paths_at_node(self,node):
-        '''
+    def paths_at_node(self, node):
+        """
         Creates a list of paths at a node
 
         Parameters
@@ -108,17 +119,17 @@ class Graph:
         If no paths found then empty list
         if paths found then returns nested list - [[node_name, distance]]
 
-        '''
+        """
         if node in self.graph.keys():
             return self.graph[node]
         return []
 
-    def add_paths(self,path_list):
-        '''
+    def add_paths(self, path_list):
+        """
         adds a list of paths to the graph
         pass graphs as nested list [[start,end, distance, one_way]]
         if one_way not passed then will default to False
-        '''
+        """
         for path in path_list:
             # Check if one_way parameter has been passed for the link
             # If not assume false
@@ -126,38 +137,38 @@ class Graph:
                 one_way = path[3]
             else:
                 one_way = False
-            self.add_path(path[0],path[1],path[2],one_way)
+            self.add_path(path[0], path[1], path[2], one_way)
 
-    def node_degree(self,node):
-        '''
+    def node_degree(self, node):
+        """
         returns how many paths leave node
-        '''
+        """
         return len(self.paths_at_node(node))
 
     def is_eulerian(self):
-        '''
+        """
         checks if an Eulerian path is possible on the graph
         Returns Boolean
-        '''
+        """
         odd_vertices = len(self.odd_vertices())
-        return (odd_vertices in {0,2})
+        return odd_vertices in {0, 2}
 
     def odd_vertices(self):
-        '''
-        returns a list of vertices that are have odd degree
-        '''
+        """
+        returns a list of vertices that have odd degree
+        """
         odd_vertex_list = []
         for node in self.graph:
-            if self.node_degree(node)%2 == 1:
+            if self.node_degree(node) % 2 == 1:
                 odd_vertex_list.append(node)
         return odd_vertex_list
 
-    def remove_path(self,start_node, end_node, length=0):
-        '''
+    def remove_path(self, start_node, end_node, length=0):
+        """
         Function looks for path from start node to end node
         Removes path and reverse.
         Finds first path if no length passed, or path matching length otherwise
-        '''
+        """
 
         # get paths to iterate through
         possible_paths = self.paths_at_node(start_node)
@@ -165,7 +176,7 @@ class Graph:
         # use first found path if distance = 0
         for path in possible_paths:
             if ((path[0] == end_node and length == path[1])
-                or (path[0] == end_node and length == 0)):
+                    or (path[0] == end_node and length == 0)):
                 self.graph[start_node].remove(path)
                 break
 
@@ -173,21 +184,21 @@ class Graph:
         possible_paths = self.paths_at_node(end_node)
         for path in possible_paths:
             if ((path[0] == start_node and length == path[1])
-                or (path[0] == start_node and length == 0)):
+                    or (path[0] == start_node and length == 0)):
                 self.graph[end_node].remove(path)
                 break
 
-    def reachable_nodes(self,node):
-        '''
+    def reachable_nodes(self, node):
+        """
         Returns a list of nodes that are reachable from given node
-        '''
+        """
         reachable_nodes = {node}
         for path in self.paths_at_node(node):
             reachable_nodes.add(path[0])
         return reachable_nodes
 
-    def connected_nodes(self,node_list):
-        '''
+    def connected_nodes(self, node_list):
+        """
         Creates a list of nodes that are connected to all the nodes passed
 
         Parameters
@@ -198,15 +209,16 @@ class Graph:
         -------
         reachable_nodes : list of reachable nodes
 
-        '''
+        """
         reachable_nodes = set(node_list)
         for node in node_list:
             for path in self.paths_at_node(node):
                 reachable_nodes.add(path[0])
         return reachable_nodes
 
+    @property
     def eulerian_path(self):
-        '''
+        """
         Function creates an Eulerian path on the graph
 
         Returns
@@ -214,14 +226,14 @@ class Graph:
         current_path : string detailing the path
         total_distance : length of path
 
-        '''
+        """
         # Determine start and finish point as a list
         start_finish_vertex = self.odd_vertices()
-        if start_finish_vertex == []:
+        if not start_finish_vertex:
             start_finish_vertex.append(list(self.graph.keys)[0])
             start_finish_vertex.append(list(self.graph.keys)[0])
 
-        #create a copy of the graph to work with during algorithm
+        # create a copy of the graph to work with during algorithm
         current_graph = Graph()
         current_graph.graph = copy.deepcopy(self.graph)
 
@@ -245,17 +257,17 @@ class Graph:
                              f'to {next_vertex}\n')
             current_graph.remove_path(current_vertex, next_vertex)
             current_vertex = next_vertex
-        return (current_path, total_distance)
+        return current_path, total_distance
 
     def fully_connected(self):
-        '''
-        Checks if a grapgh is fully connected.
+        """
+        Checks if a graph is fully connected.
 
         Returns
         -------
         bool - if graph fully connected then True
 
-        '''
+        """
         all_nodes = set(self.graph.keys())
         start_node = list(all_nodes)[0]
         current_nodes = set(start_node)
@@ -268,8 +280,8 @@ class Graph:
                 return False
 
     def dijkstra(self, start_node, end_node):
-        '''
-        Performs Djikstra's algorithm on the graph to find shortest path
+        """
+        Performs Dijkstra's algorithm on the graph to find the shortest path
         between two nodes
 
         Parameters
@@ -281,13 +293,13 @@ class Graph:
         -------
         message : string detailing path
 
-        '''
+        """
         # Create list to hold all nodes.
         # [Node name, current distance, current node, finalised?]
         all_nodes = {}
         for node in self.graph.keys():
-            all_nodes.update({node: [float('inf'),False, False]})
-        all_nodes.update({start_node:[0, True, True]})
+            all_nodes.update({node: [float('inf'), False, False]})
+        all_nodes.update({start_node: [0, True, True]})
 
         # Keep going until end_node is finalised
         while all_nodes[end_node][2] is False:
@@ -307,7 +319,7 @@ class Graph:
             lowest_node = ''
             for node in all_nodes:
                 if (all_nodes[node][2] is False
-                        and all_nodes[node][0]<lowest_distance):
+                        and all_nodes[node][0] < lowest_distance):
                     lowest_distance = all_nodes[node][0]
                     lowest_node = node
             all_nodes[lowest_node][1] = True
@@ -321,14 +333,15 @@ class Graph:
                         value_1 = float(all_nodes[path[0]][0])
                         value_2 = float(all_nodes[node][0] - path[1])
 
-                        if math.isclose(value_1 , value_2):
+                        if math.isclose(value_1, value_2):
                             message = (f'\nGo from {path[0]} to {node} '
-                                       f'- distance: {path[1]}'+ message)
-                            all_nodes[path[0]][1]=True
+                                       f'- distance: {path[1]}' + message)
+                            all_nodes[path[0]][1] = True
                             all_nodes[node][1] = False
                             all_nodes[path[0]][2] = False
                             break
         return message
+
 
 if __name__ == '__main__':
     pass
